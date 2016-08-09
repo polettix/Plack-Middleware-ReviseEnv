1873c5df9b544192f23333fa9933d23c67593a97
Flavio Poletti <flavio@polettix.it>
2016-08-09 18:45:16 +0200

    0.001001

--- lib/Plack/Middleware/ReviseEnv.pm
+++ lib/Plack/Middleware/ReviseEnv.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp qw< confess >;
 use English qw< -no_match_vars >;
-{ our $VERSION = '0.001'; }
+{ our $VERSION = '0.001001'; }
 
 use parent 'Plack::Middleware';
 
